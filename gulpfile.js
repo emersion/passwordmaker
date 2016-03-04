f749bbb16e9a590a1341163363d65e1a0d5cd85c
emersion <contact@emersion.fr>
2016-03-04 21:46:11 +0100

    Generate dependencies list automatically

--- gulpfile.js
+++ gulpfile.js
@@ -3,34 +3,39 @@ var zip = require('gulp-zip');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var browserify = require('browserify');
+var required = require('gulp-required');
 
-var dependencies = ['passwordmaker', 'url', 'debounce'];
+var b;
+
+gulp.task('required', function () {
+	b = browserify();
+
+	return gulp.src(['js/**', '!js/passwordmaker.js'])
+	.pipe(buffer())
+	.pipe(required())
+	.on('data', function (deps) {
+		b.require(deps);
+	});
+});
+
+gulp.task('browserify', ['required'], function () {
+	return b.bundle()
+	.pipe(source('passwordmaker.js'))
+	.pipe(gulp.dest('js'));
+});
 
 function files() {
 	return gulp.src(['manifest.json', '*.html', 'css/**', 'font/**', 'img/**', 'js/**'], { base: '.' });
 }
 
-gulp.task('browserify', function () {
-	var b = browserify();
-
-	dependencies.forEach(function (name) {
-		b.require(name);
-	});
-
-	return b.bundle()
-		.pipe(source('passwordmaker.js'))
-		.pipe(buffer())
-		.pipe(gulp.dest('js'));
-});
-
 gulp.task('zip', function () {
-	files()
-		.pipe(zip('passwordmaker.zip'))
-		.pipe(gulp.dest('.'));
+	return files()
+	.pipe(zip('passwordmaker.zip'))
+	.pipe(gulp.dest('.'));
 });
 
 gulp.task('xpi', function () {
-	files()
-		.pipe(zip('passwordmaker.xpi'))
-		.pipe(gulp.dest('.'));
+	return files()
+	.pipe(zip('passwordmaker.xpi'))
+	.pipe(gulp.dest('.'));
 });
